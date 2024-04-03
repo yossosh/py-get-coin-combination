93b981569f1d92964880958b160f8edc18e7326a
Sofiia <sonagm9@gmail.com>
2024-04-04 01:51:56 +0300

    'Solution1'

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,7 @@
 from app.main import get_coin_combination
 
 
-def test_get_coin_combination():
+def test_get_coin_combination() -> None:
     assert get_coin_combination(1) == [1, 0, 0, 0]
 
     assert get_coin_combination(6) == [1, 1, 0, 0]
